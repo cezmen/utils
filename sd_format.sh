7f381a3999e147b8f787c09785673869841e9c85
cezmen <cezar.menezes@live.com>
2018-02-14 15:37:24 -0200

    Update sd_format.sh

--- sd_format.sh
+++ sd_format.sh
@@ -4,13 +4,10 @@
 # Format Linux Partition
 sudo mkfs.ext3 /dev/mmcblk1p1
 
-# Mount Disk
+# Create Mounting Directory
 if [ ! -d "/media/sdcard" ]; then
   sudo mkdir /media/sdcard
 fi  
-sudo mount -t ext3 /dev/mmcblk1p1 /media/sdcard
 
 # Setup Swap Area
 sudo mkswap /dev/mmcblk1p2
-sudo swapon /dev/mmcblk1p2
-sudo swapon -s
